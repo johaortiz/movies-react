5ca0084dfccb9c64bbf8968a8fd604afd2d7d70c
johaortiz <surf234@outlook.es>
2023-01-22 03:29:06 -0300

    deploy

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,9 +4,7 @@ import styles from '../styles/App.module.css';
 export function Nav() {
     return <>
         <nav>
-            <Link to="/about">About</Link>
-            <Link to="/contact">Contact</Link>
-        </nav>
         <Link to="/"><h1 className={styles.title} >Movies</h1></Link>
+        </nav>
     </>
 }
